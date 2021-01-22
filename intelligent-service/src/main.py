0a372488f2a4616f9fdd7896087a12a43c7f7d88
Judit Kunne Tamas <judit0310@gmail.com>
2021-01-22 15:18:31 +0100

    feat(#7): added configuration of ai project. readme extended to hold the correct shell script command

--- intelligent-service/src/main.py
+++ intelligent-service/src/main.py
@@ -1,32 +1,40 @@
 """
 Main file for server
 """
-import numpy as np
+import os
 import flask
 from quadraticcontroller import quadratic_api
 from generalcontroller import general_api
 
+
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
-app.config['JSON_SORT_KEYS'] = False
-app.register_blueprint(quadratic_api)
-app.register_blueprint(general_api)
 
-def print_hi(name):
-    """ Docstring """
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+def read_config():
+    """Reads the environment variables, and validate them"""
+    arg_port = os.environ.get('PORT')
+    arg_inf_system_host = os.environ.get('IS_HOST')
+    arg_inf_system_port = os.environ.get('IS_PORT')
+    arg_delay = os.environ.get('DELAY')
+    assert arg_port is not None, "Port is missing"
+    assert arg_inf_system_host is not None, "Information system host is missing"
+    assert arg_inf_system_port is not None, "Information system port is missing"
+    assert arg_delay is not None, "Delay is missing"
+    assert arg_port.isdigit(), "Port is not a number"
+    assert arg_inf_system_port.isdigit(), "Information system port is not a number"
+    assert arg_delay.isdigit(), "Delay in not a valid number"
+    return arg_port, arg_inf_system_host, arg_inf_system_port, arg_delay
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    values = np.random.randint(0, 90, 10)
-    print(values)
-    print(values.sum())
-    app.run(host="0.0.0.0")
+    app = flask.Flask(__name__)
+    app.config["DEBUG"] = True
+    app.config['JSON_SORT_KEYS'] = False
+    app.register_blueprint(quadratic_api)
+    app.register_blueprint(general_api)
+    port, information_system_host, information_system_port, delay = read_config()
+    print("Information system is at", information_system_host + ":" + str(information_system_port))
+    app.run(host="0.0.0.0", port=port)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
